44f4802b2f5869437a0673bfd23330659f0adfde
Laio Dias <laio.dias@lunatech.nl>
2022-06-01 17:19:40 +0200

    Add repository for Customer and Order

---
+++ src/main/java/com/peecho/orderbackend/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.peecho.orderbackend.repository;
+
+import com.peecho.orderbackend.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
---
+++ src/main/java/com/peecho/orderbackend/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.peecho.orderbackend.repository;
+
+import com.peecho.orderbackend.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
